75a9c652d1f10794a93347892cf4fe31557bec34
CNH510072 <140175536+LSCNH510072@users.noreply.github.com>
2023-11-15 20:49:39 +0800

    --v 1.0.0.7

--- mian.js
+++ mian.js
@@ -1,3 +1,5 @@
+let alt = "我是图片";
+
 // 当点击按钮（class为btn的元素）时执行以下代码
 $(".btn").click(function () {
   // 切换按钮的class为active
@@ -8,12 +10,19 @@ $(".btn").click(function () {
   $(".introduce").toggleClass("ic");
 });
 
-//使用JavaScript的框架库jQuery获取免费公共的时间api然后把获取到的时间放到一个元素中
+// 当点击 .menu 下的 button 元素时执行的函数
+$(".menu button").click(function () {
+  // 切换按钮的状态
+  $(".menu button").toggleClass("button");
+  $(".blog-cover").toggleClass("active");
+});
+
+//获取免费公共的时间api然后把获取到的时间放到一个元素中
 
 $(document).ready(function () {
   // 使用jQuery的ajax方法发送GET请求获取API数据
   $.ajax({
-    url: "http://worldclockapi.com/api/json/utc/now", // 替换为您的API URL
+    url: "http://worldclockapi.com/api/json/utc/now",
     dataType: "json",
     success: function (data) {
       // 获取到API返回的时间
@@ -58,7 +67,7 @@ spanElement.addEventListener("click", function () {
   alert("已复制到剪贴板");
 });
 
-// 使用jQuery获取GitHub上的项目数量和项目名称
+// 获取GitHub上的项目数量和项目名称
 // 当页面加载完成后执行的函数
 $(document).ready(function () {
   // 获取 id 为 project 的元素
@@ -116,46 +125,57 @@ $(document).ready(function () {
   });
 });
 
-// 当点击 .menu 下的 button 元素时执行的函数
-$(".menu button").click(function () {
-  // 切换按钮的状态
-  $(".menu button").toggleClass("button");
-});
-
-
-// 定义函数
-function searchHtmlFiles(folderPath, ids) {
-  // 在指定路径下搜索文件夹找到HTML文件
+function searchBlogFiles(folderPath, ids, folderNumber) {
   $.ajax({
-    url: 'https://github.com/lscnh510072/lscnh510072.github.io/blog',
-    success: function (data) {
-      // 处理搜索到的HTML文件
-      $(data)
-        .find('a[href$=".html"]')
-        .each(function () {
-          var htmlFilePath = $(this).attr("href");
-          // 生成div并插入HTML内容
-          generateDivWithHtmlContent(htmlFilePath, ids);
-          console.log(ids);
-        });
+    url: `//leishen.weborg.top/${folderPath}/${("0" + folderNumber)}/index.html`,
+    success: function () {
+      var htmlFilePath = `//leishen.weborg.top/${folderPath}/${("0" + folderNumber)}/index.html`;
+      var jsonFilePath = `//leishen.weborg.top/${folderPath}/${("0" + folderNumber)}/disposition.json`;
+      // 继续递归搜索下一个子文件夹
+      generateBlogContent(jsonFilePath, htmlFilePath, ids, folderNumber);
+      searchBlogFiles(folderPath, ids, folderNumber + 1);
     },
   });
 }
 
 // 生成div并插入HTML内容
-function generateDivWithHtmlContent(htmlFilePath, targetElementId) {
+function generateBlogContent(
+  jsonFilePath,
+  htmlFilePath,
+  targetElementId,
+  hanshu
+) {
+  var $targetElement = $(targetElementId);
+  $.get(jsonFilePath, function (res) {
+    var $newCover_title = res.title;
+    var $newCover_img = res.img;
+    var $newCover_introduce = res.introduce;
+    let $newCover = `
+        <h1>${$newCover_title}</h1>
+        <img src="${$newCover_img}" alt="${alt}"/>
+        <p>${$newCover_introduce}</p>
+        <label>
+          <span>阅读全文</span>
+          <input type="checkbox" hidden>
+        </label>
+      `;
+    var $Cover_content = $(`<div class="blog-cover" id='${hanshu}'>`).html(
+      $newCover
+    );
+    $targetElement.after($Cover_content);
+    $(targetElementId).append($Cover_content);
+  });
+
   $.get(htmlFilePath, function (data) {
     // 获取id为xxx的元素
     console.log(targetElementId);
-    var $targetElement = $(targetElementId);
 
     // 创建一个新的div元素
-    var $newDiv = $("<div id='blog-content'>").html(data);
-
-    // 把HTML内容放到刚才生成的div元素里
-    $targetElement.after($newDiv);
+    var $newDiv = $(`<div class='blog-content'>`).html(
+      `<header><span class="iconfont icon-guanbi" style="margin-left: auto;"></span></header>${data}`
+    );
 
     // 将新的 div 元素插入到目标元素里
-    $(targetElementId).append($newDiv);
+    $(`#${hanshu} label`).append($newDiv);
   });
 }
